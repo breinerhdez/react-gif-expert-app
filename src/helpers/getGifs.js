d17d4573f1b8ef65afe2dd49050765607c677349
Breiner E. Hernández G <ing.breiner.hernandez@gmail.com>
2021-01-17 14:12:59 -0500

    fix index build

--- src/helpers/getGifs.js
+++ src/helpers/getGifs.js
@@ -1,6 +1,6 @@
 
 export const getGifs = async (category) => {
-  const url = `http://api.giphy.com/v1/gifs/search?q=${encodeURI(category)}&api_key=e8EVVoIXa7ZXcn8hEkRMj5YCSr3xE6FD&limit=10`
+  const url = `https://api.giphy.com/v1/gifs/search?q=${encodeURI(category)}&api_key=e8EVVoIXa7ZXcn8hEkRMj5YCSr3xE6FD&limit=10`
   const resp = await fetch(url)
   const { data } = await resp.json()
   const gifs = data.map(img => {
